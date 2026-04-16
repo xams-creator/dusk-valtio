f786218cb4ed23915f98cf4d37d4206cf1d19fc8
xams-creator <1228552328@qq.com>
2026-04-16 19:50:43 +0800

    upgrade: 0.1.0 feat: 优化import，导出了部分可能用到的api

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { createValtioModel } from './model';
 export { use } from './middleware';
 export { useDuskValtioSnapshot } from './hooks';
 
-export { subscribe, proxy, snapshot } from 'valtio';
-export { deepClone } from 'valtio/utils';
+export { subscribe, proxy, snapshot, useSnapshot, ref } from 'valtio';
+export * from 'valtio/utils';
